7912f5ce258dc4d73c8096e1c0b0cdf61e13f770
unknown <pallavitighare01@gmail.com>
2023-05-02 13:09:10 +0530

    functionality done

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const Environment ={
+     firebaseConfig : {
+        apiKey: "AIzaSyClt2YdwyGVDdq7Ke1y5ghNfrIsrG_Nzd0",
+        authDomain: "blog-bf6d9.firebaseapp.com",
+        projectId: "blog-bf6d9",
+        storageBucket: "blog-bf6d9.appspot.com",
+        messagingSenderId: "143341175272",
+        appId: "1:143341175272:web:f211ccb75d6c6f589a245d",
+        measurementId: "G-05WW73BFFS"
+      }
+}
